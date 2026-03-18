2eccad21138f9d82834f7fc7778ccf2dd39c2471
Masato Shuto <154576364+ms-yozakura@users.noreply.github.com>
2026-03-19 00:39:16 +0900

    update: profileセクションを追加

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 
-window.onload = function () { 
+window.onload = function () {
   timerComment();
 }
 
 const commentList = [
-"初めまして！<br>苓北町の魅力、も〜〜っと伝えるばい！",
-"苓北町は、海も山もあって自然がいっぱい！",
-"地元は苓北たい！<br>あたは？",
-"頭のは海藻じゃないけん！<br>苓北のだごうまかレタスたい！",
-"地元の人たちも優しいけん<br>温かい雰囲気が最高たい！",
-"新鮮な海の幸に美味しいレタスやみかん！食べ物も最高ばい！",
-"苓北には観光スポットもたくさんあるけん、ぜひ遊びに来てみらんね！"
+  "初めまして！<br>苓北町の魅力、も〜〜っと伝えるばい！",
+  "苓北町は、海も山もあって自然がいっぱい！",
+  "地元は苓北たい！<br>あたは？",
+  "頭のは海藻じゃないけん！<br>苓北のだごうまかレタスたい！",
+  "地元の人たちも優しいけん<br>温かい雰囲気が最高たい！",
+  "新鮮な海の幸に美味しいレタスやみかん！食べ物も最高ばい！",
+  "苓北には観光スポットもたくさんあるけん、ぜひ遊びに来てみらんね！"
 ];
 
 const comment = () => {
   const el = document.getElementById("rei-comment");
-  let num = Math.floor(Math.random() * 5);
+  let num = Math.floor(Math.random() * commentList.length);
 
   // フェードアウト
   el.classList.add("fade-out");
@@ -27,6 +27,32 @@ const comment = () => {
   }, 300); // CSSのtransitionと合わせる
 }
 
-const timerComment=()=>{
+const timerComment = () => {
   setInterval(comment, 6000);
 }
+const circles = document.querySelectorAll(".circle");
+
+const overlay = document.querySelector(".overlay");
+
+circles.forEach(circle => {
+  circle.addEventListener("click", () => {
+    const target = circle.dataset.target;
+
+    document.querySelectorAll(".modal").forEach(m => {
+      m.classList.remove("active");
+    });
+
+    document.getElementById(target).classList.add("active");
+    overlay.classList.add("active");
+
+    document.body.classList.add("no-scroll");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  document.querySelectorAll(".modal").forEach(m => {
+    m.classList.remove("active");
+  });
+  overlay.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+});
